bf823ffdc7dbbf27a21d6327fc31b8637afb561c
malek.b. <malek.boulakbech@gmail.com>
2025-09-30 12:11:00 +0200

    update form

--- src/DataForm.tsx
+++ src/DataForm.tsx
@@ -6,15 +6,20 @@ export default function DataForm(props: Readonly<DataFormProps>) {
 
     return (
         <>
+            <form>
             <div className="mb-6">
-                <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Name Eingeben:</label>
+                <label htmlFor="default-input" className="block mb-2 text-sm font-medium
+                text-gray-900">
+                    Name: </label>
                 <input
                     onChange={(e) =>
                         props.setName(e.target.value)}
                     type="text" id="default-input"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                    placeholder="Name Eingeben"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                    focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
             </div>
+            </form>
         </>
     )
 }
