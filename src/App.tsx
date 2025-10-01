2ea7e9f7da95263ebb232bfaa93d0408343b6c4a
malek.b. <malek.boulakbech@gmail.com>
2025-10-01 11:03:42 +0200

    add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,32 @@ import {Route, Routes} from "react-router-dom";
 import CharacterList from "./CharacterList.tsx";
 import Footer from "./Footer.tsx";
 import Navbar from "./Navbar.tsx";
-import {response} from "./rickandmorty.ts";
 import CharacterDetail from "./CharacterDetail.tsx";
 import CharacterForm from "./CharacterForm.tsx";
 import type {Character} from "./CharacterType.ts";
 import CharacterPage from "./CharacterPage.tsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
 
-    const characters: Character[] = response.results ;
     function onSubmit(character: Character) {
         characters.push(character);
     }
+
+    const [characters, setCharacters] = useState<Character[]>([]);
+    const url = "https://rickandmortyapi.com/api/character";
+
+    function getCharacters() {
+        axios.get(url)
+            .then(response =>
+                setCharacters(response.data.results))
+            .catch((error) => console.log(error))
+    }
+
+    useEffect(() => {
+        getCharacters();
+    }, [])
 
     return (
         <>
